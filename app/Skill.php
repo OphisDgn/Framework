755534d121d5de7cad9ecaa731414c5bab41d2ca
celine <celine@Lieutaud-Laravel.local>
2020-02-10 15:20:12 +0100

    first

---
+++ app/Skill.php
@@ -0,0 +1,15 @@
+<?php 
+
+namespace App;
+use Illuminate\Database\Eloquent\Model;
+
+class Skill extends Model {
+  	protected $fillable = array('id', 'nom', 'description', 'src');
+   
+   public function users(){
+     return $this->belongToMany('App\User')->withPivot('niveau');
+   }
+}
+
+
+?>
